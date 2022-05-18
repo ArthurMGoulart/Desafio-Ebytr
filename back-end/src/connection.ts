bda7f1e008c8a419181d59b4e8f04828f3368ab1
ArthurMGoulart <budiarthur@gmail.com>
2022-05-18 18:35:25 -0300

    fix: connection url with env variables

--- back-end/src/connection.ts
+++ back-end/src/connection.ts
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
 
 import mongoose from 'mongoose';
-
+ 
 const MONGO_DB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iafrc.mongodb.net/${process.env.NODE_ENV}?retryWrites=true&w=majority`;
 
 const connectToDatabase = (
