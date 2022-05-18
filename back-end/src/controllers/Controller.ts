56e3795e9b03cc1ace18cf1d6a9f0e53843e6b0a
ArthurMGoulart <budiarthur@gmail.com>
2022-05-18 16:35:09 -0300

    feat: General Router, Controller, Service and Model

---
+++ back-end/src/controllers/Controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import Service from '../services/Service';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+abstract class Controller<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: Service<T>) { }
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const objs = await this.service.read();
+      return res.status(200).json(objs);
+    } catch (err) {
+      const { message } = err as Error;
+      return res.status(500).json({ error: message });
+    }
+  };
+
+  abstract readOne(
+    req: Request,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+
+  abstract update(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+
+  abstract delete(
+    req: Request,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+}
+
+export default Controller;
