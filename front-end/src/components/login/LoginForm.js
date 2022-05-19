8a56391ccdfa77b07ad6cfa9aaad6a26bf2ed995
ArthurMGoulart <budiarthur@gmail.com>
2022-05-19 19:37:28 -0300

    build: setup folders and routes

---
+++ front-end/src/components/login/LoginForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate, Link } from 'react-router-dom';
+import loginValidate from './validate/loginValidate';
+import api from '../../services/api';
+
+function Login() {
+  const [invalidLogin, setInvalidLogin] = useState();
+  const { register, handleSubmit, formState: { errors, isDirty, isValid } } = useForm({
+    resolver: yupResolver(loginValidate),
+    mode: 'onChange',
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = (datas) => api.post('/login', datas)
+    .then(({ data }) => {
+      localStorage.setItem('user', JSON.stringify(data));
+      switch (data.role) {
+      case 'customer':
+        navigate('/customer/products');
+        break;
+      case 'seller':
+        navigate('/seller/orders');
+        break;
+      case 'administrator':
+        navigate('/admin/manage');
+        break;
+      default:
+        break;
+      }
+    })
+    .catch(({ response }) => setInvalidLogin(response.data));
+
+  return (
+    <form onSubmit={ handleSubmit(onSubmit) }>
+      <div>
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="common_login__input-email"
+            placeholder="Insira seu email"
+            name="email"
+            type="email"
+            { ...register('email') }
+          />
+        </label>
+        <p>{ errors.email?.message }</p>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Senha
+          <input
+            data-testid="common_login__input-password"
+            placeholder="Insira sua senha"
+            name="password"
+            type="password"
+            { ...register('password') }
+          />
+        </label>
+        <p>{ errors.password?.message }</p>
+      </div>
+      <div>
+        <p data-testid="common_login__element-invalid-email">{ invalidLogin?.message }</p>
+      </div>
+      <button
+        data-testid="common_login__button-login"
+        type="submit"
+        disabled={ !isDirty || !isValid }
+      >
+        Login
+      </button>
+
+      <button
+        onClick={ () => navigate('/register') }
+        data-testid="common_login__button-register"
+        type="button"
+      >
+        <Link to="/register">Ainda não tenho conta</Link>
+      </button>
+
+    </form>
+  );
+}
+
+export default Login;
---
+++ front-end/src/routes/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Tasks from '../pages/Tasks';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+function Routers() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route exact path="/user/tasks" element={ <Tasks /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Routers;
