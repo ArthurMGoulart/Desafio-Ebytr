90f6769510362bb9e26e5ce5cc4eea6bf3f8e22e
ArthurMGoulart <budiarthur@gmail.com>
2022-05-19 19:46:11 -0300

    feat: register and login pages

--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Tasks from '../pages/Tasks';
+// import Tasks from '../pages/Tasks';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
@@ -11,7 +11,7 @@ function Routers() {
         <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
-        <Route exact path="/user/tasks" element={ <Tasks /> } />
+        {/* <Route exact path="/user/tasks" element={ <Tasks /> } /> */}
       </Routes>
     </BrowserRouter>
   );
